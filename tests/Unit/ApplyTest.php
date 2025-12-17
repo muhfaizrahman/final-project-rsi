a74aacc90cbdafba937c21e7fc4a219f81150165
muhfaizrahman <muhammadfaizrahman07@gmail.com>
2025-12-17 20:35:36 +0700

    add another unit test

--- tests/Unit/ApplyTest.php
+++ tests/Unit/ApplyTest.php
@@ -3,11 +3,11 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ApplyTest extends TestCase
 {
-
     // protected function setUp(): void
     // {
     //     parent::setUp();
@@ -20,5 +20,29 @@ class ApplyTest extends TestCase
         $this->actingAsGuest();
         $response = $this->get('/home');
         $response->assertStatus(302);
+    }
+
+    public function test_access_apply_page_with_wrong_role(): void
+    {
+        $user = User::factory()->create([
+            'role' => 'perusahaan',
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->get('/home');
+        $response->assertForbidden();
+    }
+
+    public function test_access_apply_page_with_correct_role(): void
+    {
+        $user = User::factory()->create([
+            'role' => 'pelamar',
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->get('/home');
+        $response->assertStatus(200);
     }
 }
